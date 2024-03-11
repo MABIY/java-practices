67c0d1c5a5a9e6bc508297f0f95722e60aa049e9
lh <jyfc7879@gmail.com>
2024-03-11 13:06:00 +0800

    ...

---
+++ projects/jdbi3-example/src/main/java/org/example/Jdbi3ExampleApplication.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lh
+ */
+@SpringBootApplication
+public class Jdbi3ExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Jdbi3ExampleApplication.class, args);
+    }
+}
