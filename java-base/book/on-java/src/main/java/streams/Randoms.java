e67c7304e29588ecd32361069b9202e0a0394900
lh <jyfc7879@gmail.com>
2024-03-18 21:36:15 +0800

    ..

---
+++ java-base/book/on-java/src/main/java/streams/Randoms.java
@@ -0,0 +1,16 @@
+package streams;
+
+import java.util.Random;
+
+/**
+ * @author lh
+ */
+public class Randoms {
+    public static void main(String[] args) {
+        new Random(47).ints(5, 20)
+                .distinct()
+                .limit(7)
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
