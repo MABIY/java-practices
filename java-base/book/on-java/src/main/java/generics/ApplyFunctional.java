1a6792a33c384de370f4d4c98ede668cd31e6281
lh <jyfc7879@gmail.com>
2024-03-29 11:02:11 +0800

    ..

---
+++ java-base/book/on-java/src/main/java/generics/ApplyFunctional.java
@@ -0,0 +1,27 @@
+package generics;
+
+import onjava.Suppliers;
+
+import java.util.stream.Stream;
+
+/**
+ * @author lh
+ */
+public class ApplyFunctional {
+    public static void main(String[] args) {
+        Stream.of(Stream.generate(Shape::new).limit(2),
+                Stream.generate(Square::new).limit(2))
+                .flatMap(c -> c)
+                .peek(Shape::rotate)
+                .forEach(s-> s.resize(7));
+
+        new FilledList<>(Shape::new,2).forEach(Shape::rotate);
+        new FilledList<>(Square::new,2).forEach(Shape::rotate);
+
+        SimpleQueue<Shape> shapeQ = Suppliers.fill(
+                new SimpleQueue<>(),SimpleQueue::add,Shape::new,2
+        );
+        Suppliers.fill(shapeQ,SimpleQueue::add,Square::new,2);
+        shapeQ.forEach(Shape::rotate);
+    }
+}
